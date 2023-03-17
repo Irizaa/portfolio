20e8c72d82d48615c65e1a8cb184af043e9019a4
daniel <squaredfreezer@gmail.com>
2023-03-16 22:55:22 -0400

    finish loading screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css'
 
-function App() {
+import {React, useState, useEffect} from 'react'
+import TitleSection from './Components/TitleSection'
+import Loading from './Components/Loading'
+
+const App = () => {
+  const [loadingComplete, setLoadingComplete] = useState(false)
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        entry.target.classList.toggle('show', entry.isIntersecting);
+      })
+    })
+  const hiddenElements = document.querySelectorAll('.hidden')
+  hiddenElements.forEach((el) => observer.observe(el))
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section className = 'hidden'>
+        <TitleSection/>
+      </section>
+        {!loadingComplete && (
+            <Loading onLoadingComplete={() => setLoadingComplete(true)}></Loading>
+        )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
